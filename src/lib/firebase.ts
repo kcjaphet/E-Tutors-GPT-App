b31be1c8345aaebf8937bffb48432a15183d8e75
Kelechi  Japhet <85802894+kcjaphet@users.noreply.github.com>
2025-04-30 00:43:54 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,20 @@
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+#import { initializeApp } from "firebase/app";
+#import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBsFajLYXxEYRTx6C-5BVWGLv28E-RN61Y",
-  authDomain: "gpttexttools.firebaseapp.com",
-  projectId: "gpttexttools",
-  storageBucket: "gpttexttools.appspot.com",
-  messagingSenderId: "275536338405",
-  appId: "1:275536338405:web:63c9b2876fb24a47c743ca",
-  measurementId: "G-JF7SKEVQ5Q"
+#// Your web app's Firebase configuration
+#// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+#const firebaseConfig = {
+ # apiKey: "AIzaSyBsFajLYXxEYRTx6C-5BVWGLv28E-RN61Y",
+ # authDomain: "gpttexttools.firebaseapp.com",
+ # projectId: "gpttexttools",
+ # storageBucket: "gpttexttools.appspot.com",
+ # messagingSenderId: "275536338405",
+ # appId: "1:275536338405:web:63c9b2876fb24a47c743ca",
+ # measurementId: "G-JF7SKEVQ5Q"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export default app;
+#// Initialize Firebase
+#const app = initializeApp(firebaseConfig);
+#export const auth = getAuth(app);
+#export default app;
